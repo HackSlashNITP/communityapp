628e11bfcc27d28f69b5869d22a4ab2ce4496461
gaurav-33 <gauravs.ug23.ec@nitp.ac.in>
2024-11-18 17:06:53 +0530

    This commit is to add theme/colors/constants

---
+++ lib/utils/constants.dart
@@ -0,0 +1,25 @@
+class Constants {
+  // Padding
+  static const double paddingSmall = 8.0;
+  static const double paddingMedium = 16.0;
+  static const double paddingLarge = 24.0;
+
+  // Margin
+  static const double marginSmall = 8.0;
+  static const double marginMedium = 16.0;
+  static const double marginLarge = 24.0;
+
+  // Border Radius
+  static const double borderRadiusSmall = 4.0;
+  static const double borderRadiusMedium = 8.0;
+  static const double borderRadiusLarge = 12.0;
+
+  // Font Sizes
+  static const double fontSizeSmall = 12.0;
+  static const double fontSizeMedium = 16.0;
+  static const double fontSizeLarge = 35.0;
+
+  // Other dimensions
+  static const double appBarHeight = 56.0;
+  static const double buttonHeight = 48.0;
+}
