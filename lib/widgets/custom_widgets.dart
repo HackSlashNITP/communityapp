d89cf012d1db1d39170c18e019bb352b0442f8f8
ivar <upadhyayravi2005@gmail.com>
2024-12-18 18:31:34 +0530

    added navigation bar

---
+++ lib/widgets/custom_widgets.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:communityapp/views/home/home_view.dart';
+import 'package:communityapp/controllers/home_controller.dart';
+import 'package:communityapp/views/chat/chat_view.dart';
+import 'package:communityapp/views/learning/learning_view.dart';
+import 'package:communityapp/views/profile/profile_view.dart';
+
+class MainView extends StatelessWidget {
+  final BottomNavController bottomNavController = Get.put(BottomNavController());
+
+  final List<Widget> screens = [
+    HomeView(),
+    ChatView(),
+    LearningPage(),
+    ProfileView(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Obx(() => screens[bottomNavController.selectedIndex.value]),
+      bottomNavigationBar: Obx(() {
+        return BottomNavigationBar(
+          currentIndex: bottomNavController.selectedIndex.value,
+          onTap: (index) => bottomNavController.changeIndex(index),
+          selectedItemColor: Colors.blue,
+          unselectedItemColor: Colors.black,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.chat),
+              label: 'Chat',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.school),
+              label: 'Learning',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Profile',
+            ),
+          ],
+        );
+      }),
+    );
+  }
+}
