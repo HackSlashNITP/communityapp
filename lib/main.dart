48ce71a385834b9a6c1ffea00136e439622469b5
Nakulvarshney07 <nakulv.ug23.cs@nitp.ac.in>
2024-12-28 19:46:51 +0530

    main file changed

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ void main() async {
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const GetMaterialApp(
     debugShowCheckedModeBanner: false,
-    home: HomeView(),
+    home: MyApp(),
   ));
 }
 
